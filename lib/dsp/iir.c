e0a6d6f4160e52341929353629edff8622f96061
Sohum Datta <sohumdatta@berkeley.edu>
2016-09-17 14:44:59 -0700

    IIR implementation wrong, need to correct it

--- lib/dsp/iir.c
+++ lib/dsp/iir.c
@@ -1,11 +1,24 @@
 #include "iir.h"
+/*
+ * IIR Implementation:
+ *
+ * y[n] represents the output and x[n] the intput to the filter at instant n.
+ * The output of the filter is 
+ *
+ * y[n] = (b[0] x[n] + b[1] x[n-1] + ... + b[6] x[n-6]) - (a[0] y[n-1] + a[1] y[n-2] + ... + a[5] y [n-6])
+ *
+ * note that bo below is the first term, and ao is the second term
+ */
 
-/* Useful constants */
 
+/* Filter constants */
 static const double default_b[7] = {0.941111105720312, -5.64666663432187, 14.1166665858047, -18.8222221144062, 14.1166665858047, -5.64666663432187, 0.941111105720312};
 static const int default_b_length = 7;
 static const double default_a[6] = {-5.87861915966800, 14.4004405303014, -18.8152897328414, 13.8294247398911, -5.42164649008798, 0.885690113310109};
 static const int default_a_length = 6;
+
+static double x_values[7];
+static double y_values[6];
 
 
 /* Helper functions */
